ae5db10fbb906ccb3213ddd88ccf073287c7eed5
aditya <aditya.s@outlook.com>
2023-02-15 12:52:57 +0530

    some changes in deployment.

--- BooksLibrary/Program.cs
+++ BooksLibrary/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<BooksContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("BooksApi")));
@@ -16,11 +16,12 @@ options.UseNpgsql(builder.Configuration.GetConnectionString("BooksApi")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
 
 app.UseHttpsRedirection();
 
